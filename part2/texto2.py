cd62bb5426772613ce16c8bcf1a8a8ece08cd3d0
Cahian Freire <cahianfreire@gmail.com>
2024-07-29 19:28:30 -0300

    aula 2024-29-07

---
+++ part2/texto2.py
@@ -0,0 +1,52 @@
+# print("Joca " "Aula 50")
+
+# Converte a string para maiúsculas.
+#print('joca'.upper())
+
+# Retorna a string em ordem inversa.
+# print('joca'[::-1])
+# print(''.join(reversed('joca')))
+
+# Retorna o comprimento da string.
+# len = length = comprimento = tamanho
+# print(len('joca'))
+
+# Verifica se a string está vazia.
+# nome = ''
+# print(nome == '')
+
+
+# nacional R$ 1.000.000,00
+# internacional R$ 1,000,000.00
+
+# f-string
+# format-string
+# formatar um texto
+# num = 999.999
+# print(f"O número que o Joca escolheu {num}.")
+# print(f"Mais um: {num + 1}.")
+
+# função -> retorna alguma valor
+# print(len("joaquim gomes"))  # 12
+# print(len("joaquimgomes"))   # 11
+
+# print(len("")) => 0
+# print(""[0]) => IndexError: string index out of range
+
+# print(len("j"))
+# print("j"[0])
+
+# print(len("abc")) => 3
+# print("abc"[2]) => 'c'
+
+# print("abc"['a']) => IndexError: string index out of range
+# print("abc"[]) => IndexError: string index out of range
+
+# len => começa contando com 1
+# []  => começa contando com 0
+
+# print(len("6"))
+# print("b"[0])
+
+# print(len("iphone"))
+print("iphone"[3])
