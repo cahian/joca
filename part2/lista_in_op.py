d9baecdcb74696b2f043e4d8e889287d2f8c6193
Cahian Freire <cahianfreire@gmail.com>
2024-09-11 10:58:56 -0300

    aula 2024-09-11

---
+++ part2/lista_in_op.py
@@ -0,0 +1,26 @@
+# in oq significa?
+
+# in = em, no, na, nos, nas, está contido em
+# in retorna True ou False
+
+# print = ....., imprimir
+
+# jogadores de basquete
+basquete = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# print(17 in basquete)
+# print(7 in basquete)
+# print(2 in basquete)
+
+# jogadores de futebol
+jogadores_de_futebol = [11, 8, 13, 14, 16, 10, 7, 13, 3]
+
+# print(7 in jogadores_de_futebol)
+# print(16 in jogadores_de_futebol and 27 in jogadores_de_futebol)
+# print(12 in jogadores_de_futebol or 9 in jogadores_de_futebol)
+# print(
+# 	8 in jogadores_de_futebol
+# 	and 14 in jogadores_de_futebol
+# 	and 7 in jogadores_de_futebol
+# )
+# print(32 not in jogadores_de_futebol)
